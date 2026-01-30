32d93dcd0c66cd60b5a3f893cfdae26c21d09a46
Rabi Khatri Chhetri <rabichhetri189@gmail.com>
2026-01-30 10:36:29 +0545

    Basic Steup and Understanding

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Dog from './components/Dog'
+import { Canvas } from '@react-three/fiber'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-  
+    <Canvas>
+      <Dog/>
+    </Canvas>
     </>
   )
 }

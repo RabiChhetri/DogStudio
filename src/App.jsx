043b9554a48cbeffa4b9f8a985e6a5cac1fbabc5
Rabi Khatri Chhetri <rabichhetri189@gmail.com>
2026-01-31 10:45:54 +0545

    Background changed in section-2

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,17 @@ function App() {
   return (
     <>
       <main>
+        <div className="images">
+          <img id="tomorrowland" src="/tommorowland.png" alt="" />
+          <img id="navy-pier" src="/navy-pier.png" alt="" />
+          <img id="msi-chicago" src="/msi-chicago.png" alt="" />
+          <img id="phone" src="/phone.png" alt="" />
+          <img id="kikk" src="/kikk.png" alt="" />
+          <img id="kennedy" src="/kennedy.png" alt="" />
+          <img id="opera" src="/opera.png" alt="" />
+        </div>
         <Canvas
+        id='canvas-elem'
           style={{
             height: "100vh",
             width: "100vw",
@@ -17,9 +27,6 @@ function App() {
             top: 0,
             left: 0,
             zIndex: 1,
-            backgroundImage: "url(/background-l.png)",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
           }}
         >
           <Dog />
@@ -79,31 +86,31 @@ function App() {
         </section>
         <section id="section-2">
           <div className="titles">
-            <div className="title">
+            <div img-title="tomorrowland" className="title">
               <small>2020 - ONGOING</small>
               <h1>Tomorrowland</h1>
             </div>
-            <div className="title">
+            <div img-title="navy-pier" className="title">
               <small>2020 - ONGOING</small>
               <h1>Navy Pier</h1>
             </div>
-            <div className="title">
+            <div img-title="msi-chicago" className="title">
               <small>2020 - ONGOING</small>
               <h1>MSI Chicago</h1>
             </div>
-            <div className="title">
+            <div img-title="phone" className="title">
               <small>2020 - ONGOING</small>
               <h1>This WaS louise's Phone</h1>
             </div>
-            <div className="title">
+            <div img-title="kikk" className="title">
               <small>2020 - ONGOING</small>
               <h1>KIKK Festival 2018</h1>
             </div>
-            <div className="title">
+            <div img-title="kennedy"  className="title">
               <small>2020 - ONGOING</small>
               <h1>The Kenned Center</h1>
             </div>
-            <div className="title">
+            <div img-title="opera" className="title">
               <small>2020 - ONGOING</small>
               <h1>Royal Opera Of Wallonia</h1>
             </div>
